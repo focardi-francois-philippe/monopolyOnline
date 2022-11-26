3d96cba40a916121fbf6ebdeeb7095a7b109da3d
Focardi François-Philippe <francois.focardi@hotmail.fr>
2022-11-26 12:07:19 +0100

    test

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const port = 3000;
+const port = 8080;
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
